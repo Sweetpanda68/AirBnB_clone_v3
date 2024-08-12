ac7511927a4238559f3319057e37da8afdf302bd
Dawit Tadesse <dawittadesse927@gmail.com>
2024-08-12 23:39:04 +0300

    task: 4

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -10,13 +10,14 @@ from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/2-hbnb/', strict_slashes=False)
+@app.route('/3-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
